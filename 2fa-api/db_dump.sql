1757c6aaefd21613f0b1d882c2327f2c10f3c82b
James Gill <james@jgill.dev>
2021-08-09 19:33:13 -0700

    Add unique constraint to email and phone in db dump

--- 2fa-api/db_dump.sql
+++ 2fa-api/db_dump.sql
@@ -24,9 +24,9 @@ DROP TABLE IF EXISTS `Users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL UNIQUE,
   `password` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
