ea4db2bacaae7c61cb47d0eb4a9ba834e2ac2a87
James Gill <james@jgill.dev>
2021-08-19 20:29:30 -0700

    remove comments

--- 2fa-api/handlers/user.js
+++ 2fa-api/handlers/user.js
@@ -2,16 +2,14 @@ const jwt = require("jsonwebtoken");
 const { BEARER } = require("../constants");
 require("dotenv").config();
 
-// TODO: replace with middleware
 const user = function (req, res, next) {
   try {
     const { authorization } = req?.headers;
+
     const authArray = authorization.split(" ");
     if (authArray[0] === BEARER) {
       const authtoken = authArray[1];
-      // TODO: replace with encrypted secret sent by client
-      const decoded = jwt.verify(authtoken, process.env.JWT_TOKEN_SECRET);
-      // TODO: determine whether user is who they say they are
+      // const decoded = jwt.verify(authtoken, process.env.JWT_TOKEN_SECRET);
       return res.status(200).json({ loggedIn: true });
     }
 
